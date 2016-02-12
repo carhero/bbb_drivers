99c95cf5131a09e4aba31dc03a3968435350b19d
23ars <ardeleanasm@gmail.com>
2016-02-12 22:18:33 +0200

    removed temporary files

--- gpio_driver/.bbbgpio.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/mihai/Git/bbb_drivers/gpio_driver/bbbgpio.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.16.0-4-common/scripts/module-common.lds --build-id  -o /home/mihai/Git/bbb_drivers/gpio_driver/bbbgpio.ko /home/mihai/Git/bbb_drivers/gpio_driver/bbbgpio.o /home/mihai/Git/bbb_drivers/gpio_driver/bbbgpio.mod.o
