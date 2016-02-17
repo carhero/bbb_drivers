8f24b6f6f9fb18c94bc86296179f2597215f33bf
23ars <ardeleanasm@gmail.com>
2016-02-17 14:40:49 +0200

    BBB Gpio driver test #3

--- gpio_driver/test.c
+++ gpio_driver/test.c
@@ -15,8 +15,9 @@ gcc test.c -o test
 /*P9.41 for write and P8.17 for read*/
 #define GROUP_WRITE 0
 #define PIN_NO_WRITE 20
-#define GROUP_READ 2//0
-#define PIN_NO_READ 2//27
+#define GPIO_NUMBER_WRITE 32*GROUP_WRITE+PIN_NO_WRITE
+#define GROUP_READ 0
+#define PIN_NO_READ 27
 #define NO_OF_WRITES 10
 #define NO_OF_READS 10
 int main(int argc, char argv)
@@ -25,6 +26,9 @@ int main(int argc, char argv)
       struct bbbgpio_ioctl_struct ioctl_struct;
       int i;
       int read_value;
+      int irq;
+      const uint16_t gpio_number_write=32*GROUP_WRITE+PIN_NO_WRITE;
+      const uint16_t gpio_number_read=32*GROUP_READ+PIN_NO_READ;
       fd=open("/dev/bbbgpio0",O_RDWR);
       if(fd==-1){
             fprintf(stderr,"Open:%s\n",strerror(errno));
@@ -32,70 +36,122 @@ int main(int argc, char argv)
       }
       /* Fill with 0s structure*/
       memset(&ioctl_struct,0,sizeof(struct bbbgpio_ioctl_struct));
-      
-      /*Enable busy wait mode*/
-      if(ioctl(fd,IOCBBBGPIOSBW,&ioctl_struct)!=0){
-            fprintf(stderr,"IOCBBBGPIOSBW:%s\n",strerror(errno));
+      /* Request pin*/
+      ioctl_struct.gpio_number=gpio_number_write;
+      if(ioctl(fd,IOCBBBGPIORP,&ioctl_struct)!=0){
+            fprintf(stderr,"IOCBBBGPIORP:%s\n",strerror(errno));
+            goto error;
+      }
+      ioctl_struct.gpio_number=gpio_number_read;
+      if(ioctl(fd,IOCBBBGPIORP,&ioctl_struct)!=0){
+            fprintf(stderr,"IOCBBBGPIORP:%s\n",strerror(errno));
             goto error;
       }
       
-      // /* Set direction output for writes*/
-      // ioctl_struct.gpio_group=GROUP_WRITE;
-      // ioctl_struct.write_buffer=(0xFFFFFFFF-(1<<PIN_NO_WRITE));
-      // if(ioctl(fd,IOCBBBGPIOSD,&ioctl_struct)!=0){
-      //       fprintf(stderr,"IOCBBBGPIOSD:%s\n",strerror(errno));
-      //       goto error;
-      // }
-      // /*Perform NO_OF_WRITES with toggle*/
-      // i=0;
-      // while(i<NO_OF_WRITES){
-      //       ioctl_struct.write_buffer=(1<<PIN_NO_WRITE);
-      //       if(ioctl(fd,IOCBBBGPIOWR,&ioctl_struct)!=0){
-      //             fprintf(stderr,"IOCBBBGPIOWR:%s\n",strerror(errno));
-      //             goto error;
-      //       }
-      //       sleep(1);
-      //       ioctl_struct.write_buffer=(0xFFFFFFFF-(1<<PIN_NO_WRITE));
-      //       if(ioctl(fd,IOCBBBGPIOWR,&ioctl_struct)!=0){
-      //             fprintf(stderr,"IOCBBBGPIOWR:%s\n",strerror(errno));
-      //             goto error;
-      //       }
-      //       sleep(1);
-      //       i++;
-      // }
-      
-      /*Perform NO_OF_WRITES without ioctl*/
-      // i=0;
-      // while(i<NO_OF_WRITES){
-      //       ioctl_struct.write_buffer=(1<<PIN_NO_WRITE);
-      //       write(fd,&ioctl_struct,sizeof(struct bbbgpio_ioctl_struct));
-      //       sleep(1);
-      //       ioctl_struct.write_buffer=(0xFFFFFFFF-(1<<PIN_NO_WRITE));
-      //       write(fd,&ioctl_struct,sizeof(struct bbbgpio_ioctl_struct));
-      //       sleep(1);
-      //       i++;
-      // }
-      
-      /*Set direction input (capture)*/
-      ioctl_struct.gpio_group=GROUP_READ;
-      ioctl_struct.write_buffer=(1<<PIN_NO_READ);
+
+      /* Set direction output for writes*/
+      ioctl_struct.gpio_number=gpio_number_write;
+      ioctl_struct.write_buffer=1;
       if(ioctl(fd,IOCBBBGPIOSD,&ioctl_struct)!=0){
             fprintf(stderr,"IOCBBBGPIOSD:%s\n",strerror(errno));
             goto error;
       }
-      
-      /*Perform read with ioctl*/
+      /*Perform NO_OF_WRITES with toggle*/
       i=0;
-      while(i<NO_OF_READS){
-            
-            // read(fd,&ioctl_struct,sizeof(struct bbbgpio_ioctl_struct));
-            if(ioctl(fd,IOCBBBGPIORD,&ioctl_struct)!=0){
+      while(i<NO_OF_WRITES){
+            ioctl_struct.gpio_number=gpio_number_write;
+            ioctl_struct.write_buffer=1;
+            if(ioctl(fd,IOCBBBGPIOWR,&ioctl_struct)!=0){
                   fprintf(stderr,"IOCBBBGPIOWR:%s\n",strerror(errno));
                   goto error;
             }
+            sleep(1);
+            ioctl_struct.gpio_number=gpio_number_write;
+            ioctl_struct.write_buffer=0;
+            if(ioctl(fd,IOCBBBGPIOWR,&ioctl_struct)!=0){
+                  fprintf(stderr,"IOCBBBGPIOWR:%s\n",strerror(errno));
+                  goto error;
+            }
+            sleep(1);
+            i++;
+      }
+      
+      /*Perform NO_OF_WRITES without ioctl*/
+      i=0;
+      while(i<NO_OF_WRITES){
+            ioctl_struct.gpio_number=gpio_number_write;
+            ioctl_struct.write_buffer=1;
+            write(fd,&ioctl_struct,sizeof(struct bbbgpio_ioctl_struct));
+            sleep(1);
+            ioctl_struct.gpio_number=gpio_number_write;
+            ioctl_struct.write_buffer=0;
+            write(fd,&ioctl_struct,sizeof(struct bbbgpio_ioctl_struct));
+            sleep(1);
+            i++;
+      }
+      
+      /*Set direction input (capture)*/
+      ioctl_struct.gpio_number=gpio_number_read;
+      ioctl_struct.write_buffer=0;
+      if(ioctl(fd,IOCBBBGPIOSD,&ioctl_struct)!=0){
+            fprintf(stderr,"IOCBBBGPIOSD:%s\n",strerror(errno));
+            goto error;
+      }
+      /*Perform read*/
+      i=0;
+      while(i<NO_OF_READS){
+            
+            read(fd,&ioctl_struct,sizeof(struct bbbgpio_ioctl_struct));
             printf("0x%08X\n",ioctl_struct.read_buffer);
             sleep(1);
             i++;
+      }
+      
+      /*Enable interrupts*/
+      ioctl_struct.gpio_number=gpio_number_read;
+      if(ioctl(fd,IOCBBBGPIOSH1,&ioctl_struct)!=0){
+            fprintf(stderr,"IOCBBBGPIOSH1:%s\n",strerror(errno));
+            goto error;
+      }
+     
+      ioctl_struct.gpio_number=gpio_number_read;
+      if(ioctl(fd,IOCBBBGPIOSIN,&ioctl_struct)!=0){
+            fprintf(stderr,"IOCBBBGPIOSD:%s\n",strerror(errno));
+            goto error;
+      }
+      irq=ioctl_struct.irq_number;
+      i=0;
+      while(i<NO_OF_READS){
+            ioctl_struct.gpio_number=gpio_number_read;
+            ioctl_struct.irq_number=irq;
+            if(ioctl(fd,IOCBBBGPIORD,&ioctl_struct)!=0){
+                  fprintf(stderr,"IOCBBBGPIORD:%s\n",strerror(errno));
+                  goto error;
+            }
+            printf("0x%08X\n",ioctl_struct.read_buffer);
+            sleep(2);
+            i++;
+      }
+      
+      
+      ioctl_struct.gpio_number=gpio_number_read;
+      ioctl_struct.irq_number=irq;
+      printf("Irq number %i",irq);
+      if(ioctl(fd,IOCBBBGPIOSBW,&ioctl_struct)!=0){
+            fprintf(stderr,"IOCBBBGPIOSD:%s\n",strerror(errno));
+            goto error;
+      }
+      
+      /* Release pin*/
+      ioctl_struct.gpio_number=gpio_number_read;
+      if(ioctl(fd,IOCBBBGPIOUP,&ioctl_struct)!=0){
+            fprintf(stderr,"IOCBBBGPIOUP:%s\n",strerror(errno));
+            goto error;
+      }
+      ioctl_struct.gpio_number=gpio_number_write;
+      if(ioctl(fd,IOCBBBGPIOUP,&ioctl_struct)!=0){
+            fprintf(stderr,"IOCBBBGPIOUP:%s\n",strerror(errno));
+            goto error;
       }
       close(fd);
       return 0;
